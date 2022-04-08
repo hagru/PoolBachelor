39c8803d38ce8bfa0ec9e1ff3b6e71f2b73779b6
Hagru <christianhagrupsen@gmail.com>
2022-04-08 12:15:20 +0200

    New attempt

--- PoolDesktopApp-master/DesktopApp.Designer.cs
+++ PoolDesktopApp-master/DesktopApp.Designer.cs
@@ -318,7 +318,8 @@ namespace PoolDesktopApp
             this.pboLoading.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.pboLoading.BackColor = System.Drawing.Color.Transparent;
             this.pboLoading.Enabled = false;
-            this.pboLoading.Location = new System.Drawing.Point(1249, 1324);
+            this.pboLoading.Image = ((System.Drawing.Image)(resources.GetObject("pboLoading.Image")));
+            this.pboLoading.Location = new System.Drawing.Point(1240, 1300);
             this.pboLoading.Name = "pboLoading";
             this.pboLoading.Size = new System.Drawing.Size(83, 83);
             this.pboLoading.TabIndex = 70;
