dbe9166ef9edbfa8f0c72b4be815b8911784e4c6
Hagru <christianhagrupsen@gmail.com>
2022-05-03 19:56:59 +0200

    Changes to design to fit display

--- PoolDesktopApp-master/SimulationApp.Designer.cs
+++ PoolDesktopApp-master/SimulationApp.Designer.cs
@@ -101,6 +101,7 @@ namespace PoolDesktopApp
             // 
             // lblP2
             // 
+            this.lblP2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.lblP2.BackColor = System.Drawing.Color.Transparent;
             this.lblP2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblP2.ForeColor = System.Drawing.Color.White;
