84cb473b720e0582ae993dbb4e11a10c9c5db68f
Hagru <christianhagrupsen@gmail.com>
2022-05-04 14:57:52 +0200

    Resize

--- PoolDesktopApp-master/DesktopApp.Designer.cs
+++ PoolDesktopApp-master/DesktopApp.Designer.cs
@@ -486,6 +486,9 @@ namespace PoolDesktopApp
             // 
             // pictureBox2
             // 
+            this.pictureBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
             this.pictureBox2.Location = new System.Drawing.Point(0, 0);
             this.pictureBox2.Name = "pictureBox2";
